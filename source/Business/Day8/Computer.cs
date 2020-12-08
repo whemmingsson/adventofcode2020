5eeb2ef12c97c5f9734c97ad0d8f8f1b41a8188c
William <william.hemmingsson@gmail.com>
2020-12-08 23:03:28 +0100

    Day 8 refactoring

---
+++ source/Business/Day8/Computer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020.Business.Day8
+{
+    internal class Computer
+    {
+        private Hashtable executedInstructions;
+        private int pointer;
+        private Dictionary<string, Action<int>> operations;
+
+        public enum ExitCode
+        {
+            Success = 0, InfiniteLoop = 1
+        }
+
+        public int Accumulator { get; set; }
+
+        public Computer() { }
+
+        public ExitCode Run(List<string> instructions)
+        {
+            Reset();
+
+            while(true)
+            {
+                if (pointer >= instructions.Count)
+                    return ExitCode.Success;
+
+                if (executedInstructions.ContainsKey(pointer))
+                    return ExitCode.InfiniteLoop;
+
+                ExecuteInstruction(ParseInstruction(instructions[pointer]));             
+            } 
+
+            void ExecuteInstruction((string op, int value) instruction)
+            {
+                executedInstructions.Add(pointer, true);
+                operations[instruction.op](instruction.value);               
+            }
+        }
+
+        private void Reset()
+        {
+            Accumulator = 0;
+            executedInstructions = new Hashtable();
+            pointer = 0;
+
+            operations = new Dictionary<string, Action<int>> 
+            {
+                { "acc", v => { Accumulator += v; pointer++; } },
+                { "jmp", v => pointer += v },
+                { "nop", v => pointer++ }
+            };
+        }
+
+        public static string GetOperation(string instruction)
+        {
+            return ParseInstruction(instruction).op;
+        }
+
+        private static (string op, int value) ParseInstruction(string instruction)
+        {
+            var instructionParts = instruction.Split(" ");
+            return (instructionParts[0], int.Parse(instructionParts[1]));
+        }
+    }
+}
--- source/Solutions/Day8.cs
+++ source/Solutions/Day8.cs
@@ -1,9 +1,7 @@
 ﻿using AdventOfCode2020.Business;
+using AdventOfCode2020.Business.Day8;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020.Solutions
 {
@@ -27,36 +25,35 @@ namespace AdventOfCode2020.Solutions
 
         public int SolvePart1()
         {
-            var program = new AdventOfCode2020.Business.Day8.Program(Data);
-
-            program.Run();
-
-            return program.Accumulator;
+            var computer = new Computer();
+            computer.Run(Data);
+            return computer.Accumulator;
         }
 
         public int SolvePart2()
         {
-            var program = new AdventOfCode2020.Business.Day8.Program();
+            var computer = new Computer();
 
-            for (int i = 0; i < Data.Count; i++)
+            for (var i = 0; i < Data.Count; i++)
             {
                 if (!Data[i].Contains("jmp") && !Data[i].Contains("nop"))
                     continue;
 
                 var programCode = Data.Select(l => l).ToList();
-         
-                if (Data[i].Contains("jmp"))
-                    programCode[i] = Data[i].Replace("jmp", "nop");
-                else if (Data[i].Contains("nop"))
-                    programCode[i] = Data[i].Replace("nop", "jmp");
 
-                program.SetInstructions(programCode);
-                
-                if(program.Run() == 0)
-                    return program.Accumulator;
+                var op = Computer.GetOperation(Data[i]);
+                programCode[i] = Data[i].Replace(op, OtherInstruction(op));
+
+                if(computer.Run(programCode) == Computer.ExitCode.Success)
+                    return computer.Accumulator;
             }
 
             return -1;
+
+            static string OtherInstruction(string instruction)
+            {
+                return instruction == "jmp" ? "nop" : "jmp";
+            }
         }
     }
 }
