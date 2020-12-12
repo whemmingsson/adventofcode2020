8103bf2a464225772e6ef84632aa1e3e959d5627
William <william.hemmingsson@gmail.com>
2020-12-12 21:28:11 +0100

    Solved day 12 part 1

---
+++ source/Business/Day12/Ferry.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace AdventOfCode2020.Business.Day12
+{
+    public class Ferry
+    {
+        public enum Direction
+        {
+            North = 78, South = 83, East = 69, West = 87,
+            Left = 76, Right = 82,
+            Forward = 70
+        }
+
+        private class Position
+        {
+            public Position(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        public Direction CurrentDirection { get; set; }
+        private Position CurrentPosition { get; set; }
+
+        public Ferry()
+        {
+            CurrentDirection = Direction.East;
+            CurrentPosition = new Position(0, 0);
+        }
+
+        internal void Move((Direction, int) ins)
+        {
+            switch (ins.Item1)
+            {
+                case Direction.East: CurrentPosition.X += ins.Item2; break;
+                case Direction.West: CurrentPosition.X -= ins.Item2; break;
+                case Direction.North: CurrentPosition.Y -= ins.Item2; break;
+                case Direction.South: CurrentPosition.Y += ins.Item2; break;
+                case Direction.Left: Rotate(Direction.Left, ins.Item2); break;
+                case Direction.Right: Rotate(Direction.Right, ins.Item2); break;
+                case Direction.Forward: Move((CurrentDirection, ins.Item2)); break;
+            }
+        }
+
+        private void Rotate(Direction direction, int degrees)
+        {
+            if (direction == Direction.Left)
+                degrees = 360 - degrees;
+
+            for (var i = 0; i < degrees / 90; i++)
+            {
+                CurrentDirection = Next(CurrentDirection);
+            }
+        }
+
+        private Direction Next(Direction d)
+        {
+            if (d == Direction.North) return Direction.East;
+            if (d == Direction.East) return Direction.South;
+            if (d == Direction.South) return Direction.West;
+            if (d == Direction.West) return Direction.North;
+
+            return Direction.East;
+        }
+
+        public int GetTravelDistance()
+        {
+            return Math.Abs(CurrentPosition.X) + Math.Abs(CurrentPosition.Y);
+        }
+    }
+}
---
+++ source/Solutions/Day12.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode2020.Business;
+using AdventOfCode2020.Business.Day12;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static AdventOfCode2020.Business.Day12.Ferry;
+
+namespace AdventOfCode2020.Solutions
+{
+    internal partial class Day12 : CodePuzzleSolution<string>
+    {
+        private Ferry ferry;
+     
+        public Day12(bool autoSolve = true, bool time = false) : base(12, new LineParser<string>(), autoSolve, time) { }
+
+        public override void Solve()
+        {
+            if (!AutoSolve || Data == null || !Data.Any())
+                return;
+
+            Console.WriteLine("--- Day 12: Rain Risk ----");
+            Console.WriteLine($"Part 1: {SolveWithFun(SolvePart1)}");
+            Console.WriteLine($"Part 2: {SolveWithFun(SolvePart2)}");
+            Console.WriteLine("");
+        }
+
+        public int SolvePart1()
+        {
+            ferry = new Ferry();
+
+            var instructions = Data.Select(line => ((Direction)(line.Substring(0, 1)[0]), int.Parse(line.Substring(1))));
+
+            foreach(var ins in instructions)
+            {
+                ferry.Move(ins);
+            }
+
+            return ferry.GetTravelDistance();   
+        }
+
+        public int SolvePart2()
+        {
+            return -1;
+        }
+    }
+}
