fc7671c5bb1d37f9bab50f6b516a275058912d03
William <william.hemmingsson@gmail.com>
2020-12-14 22:02:27 +0100

    Solved day 14 part 2

--- source/Solutions/Day14.cs
+++ source/Solutions/Day14.cs
@@ -9,7 +9,10 @@ namespace AdventOfCode2020.Solutions
 {
     internal partial class Day14 : CodePuzzleSolution<string>
     {
+        private const int MASK_LENGTH = 36;
+
         private Dictionary<long, long> memory;
+
         public Day14(bool autoSolve = true, bool time = false) : base(14, new LineParser<string>(), autoSolve, time) { }
 
         public override void Solve()
@@ -51,19 +54,6 @@ namespace AdventOfCode2020.Solutions
             return SumMemory();
         }
 
-        private void WriteToMemory(long value, long adress)
-        {
-            if (memory.ContainsKey(adress))
-                memory[adress] = value;
-            else
-                memory.Add(adress, value);
-        }
-
-        private long SumMemory()
-        {
-            return memory.Values.Sum(v => v);
-        }
-
         public long SolvePart2()
         {
             memory = new Dictionary<long, long>();
@@ -78,14 +68,13 @@ namespace AdventOfCode2020.Solutions
                 {
                     mask = maskRegex.Match(line).Value.Trim();
                 }
-
-                if (line.Contains("mem"))
+                else if (line.Contains("mem"))
                 {
                     var groups = memRegex.Match(line).Groups;
                     var value = int.Parse(groups[2].Value);
                     var adress = uint.Parse(groups[1].Value);
 
-                    foreach(var a in ApplyMask(mask, adress))
+                    foreach (var a in GetAllAdresses(mask, adress))
                     {
                         WriteToMemory(value, a);
                     }
@@ -95,41 +84,55 @@ namespace AdventOfCode2020.Solutions
             return SumMemory();
         }
 
-        private List<long> ApplyMask(string mask, long adress)
+        private void WriteToMemory(long value, long adress)
         {
-            var numOfFloatingBits = mask.Count(m => m == 'X');
-
-            var result = new List<long>();
-
-            var maskedAdress = ApplyOrMask(adress, mask);
-            
-            foreach(var e in EnumCalculator.GetEnumerations(numOfFloatingBits))
-            {
-                var currentBit = 0;
-                var newAdress = maskedAdress.ToCharArray();
-                for(var i = 0; i < maskedAdress.Length; i++)
-                {
-                    if (maskedAdress[i] != 'X') continue;
-
-                    newAdress[i] = e[currentBit];
-                    currentBit++;
-                }
-                result.Add((Convert.ToInt64(new string(newAdress), 2)));
-            }
-
-            return result;
+            if (memory.ContainsKey(adress))
+                memory[adress] = value;
+            else
+                memory.Add(adress, value);
         }
 
-        private string ApplyOrMask(long value, string mask)
+        private long SumMemory()
         {
-            var binaryValue = Convert.ToString(value, 2);
-            var maskTrimmed = mask.Substring(mask.Length - binaryValue.Length, binaryValue.Length);
+            return memory.Values.Sum(v => v);
+        }
+
+        private IEnumerable<long> GetAllAdresses(string mask, long adress)
+        {
+            var maskedAdress = ApplyMask(adress, mask);
+            
+            foreach(var e in EnumCalculator.GetEnumerations(mask.Count(m => m == 'X')))
+            {     
+                yield return Convert.ToInt64(CreateAddress(maskedAdress, e), 2);
+            }
+        }
+
+        private string CreateAddress(string adressTemplate, string binaryEnum)
+        {
+            var newAdress = adressTemplate.ToCharArray();
+            var currentBitPos = 0;
+
+            for (var i = 0; i < MASK_LENGTH; i++)
+            {
+                if (adressTemplate[i] != 'X') continue;
+
+                newAdress[i] = binaryEnum[currentBitPos];
+                currentBitPos++;
+            }
+
+            return new string(newAdress);
+        }
+
+        private string ApplyMask(long value, string mask)
+        {       
+            // Pad the value with zeros to make the replacement masking easier
+            var binaryValue = GetPaddedBinary(value);
 
             var resultBinary = "";
 
-            for(int i = 0; i < binaryValue.Length; i++)
+            for (int i = 0; i < MASK_LENGTH; i++)
             {
-                var maskBit = maskTrimmed[i];
+                var maskBit = mask[i];
                 var valueBit = binaryValue[i];
 
                 if (maskBit == 'X')
@@ -137,10 +140,23 @@ namespace AdventOfCode2020.Solutions
                 else if (maskBit == '1')
                     resultBinary += '1';
                 else
-                    resultBinary += valueBit;             
+                    resultBinary += valueBit;
             }
 
             return resultBinary;
+        }
+
+        private static string GetPaddedBinary(long value)
+        { 
+            var binaryValue = Convert.ToString(value, 2);
+            var padLength = MASK_LENGTH - binaryValue.Length;
+
+            for (var i = 0; i < padLength; i++)
+            {
+                binaryValue = "0" + binaryValue;
+            }
+
+            return binaryValue;
         }
     }
 }
